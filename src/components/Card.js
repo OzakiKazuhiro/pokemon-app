ab244fb52ba354731f2704145693602b28525904
OzakiKazuhiro <sciencegreen1110@gmail.com>
2024-11-22 08:39:53 +0900

    11/22

---
+++ src/components/Card.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Card.css";
+
+const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardImg">
+        {" "}
+        <img src={pokemon.sprites.front_default} alt="" />
+      </div>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((typeArray, i) => {
+          return (
+            <div key={i}>
+              <span className="typeName">{typeArray.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p>重さ：{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p>高さ：{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p>アビリティ：{pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
