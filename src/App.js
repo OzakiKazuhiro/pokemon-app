28ba18f9b702c691a9e11f594f4e86ba1bb86e0c
OzakiKazuhiro <sciencegreen1110@gmail.com>
2024-12-29 01:39:40 +0900

    12/29

--- src/App.js
+++ src/App.js
@@ -33,7 +33,75 @@ function App() {
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
       data.map((pokemon) => {
-        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const loadPokemonGoldSilverRubySapphire = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(11).map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+  const loadPokemonDiamondPearl = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(6).map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const loadPokemonBlackWhite = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(14).map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const loadPokemonXYSwordShield = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(9).map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const loadPokemonSunMoon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(1).map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const loadPokemonArceus = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(18).map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const loadPokemonScarletViolet = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.slice(5).map((pokemon) => {
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
@@ -62,6 +130,113 @@ function App() {
     setLoading(false);
   };
 
+  const handleGoldSilverPage = async () => {
+    const goldSilverPageUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=140&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(goldSilverPageUrl);
+    console.log(data.previous);
+    await loadPokemonGoldSilverRubySapphire(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleRubySapphirePage = async () => {
+    const RubySapphireUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=240&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(RubySapphireUrl);
+    console.log(data.previous);
+    await loadPokemonGoldSilverRubySapphire(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleDiamondPearl = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=380&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonDiamondPearl(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleBlackWhite = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=480&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonBlackWhite(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleXY = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=640&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonXYSwordShield(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+  const handleSunMoon = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=720&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonSunMoon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleSwordShield = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=800&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonXYSwordShield(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleArceus = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=880&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonArceus(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleScarletViolet = async () => {
+    const DiamondPealUrl =
+      "https://pokeapi.co/api/v2/pokemon?offset=900&limit=20";
+    setLoading(true);
+    let data = await getURLtoJson(DiamondPealUrl);
+    console.log(data.previous);
+    await loadPokemonScarletViolet(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
   return (
     <>
       <Navbar />
@@ -70,7 +245,7 @@ function App() {
           <h1>ロード中・・・</h1>
         ) : (
           <>
-            <div className="gameBoyBody"></div>
+            {/* <div className="gameBoyBody"></div>
             <div className="gameBoyFrame__redLine"></div>
             <div className="gameBoyFrame"></div>
             <div className="gameBoyFrame__redLine"></div>
@@ -81,7 +256,7 @@ function App() {
             <div className="gameBoyFrame__batteryLight"></div>
             <div className="gameBoyFrame__batteryMessage">Battery</div>
             <div className="gameBoyFrame__nintenbo">Nintenbo</div>
-            <div className="gameBoyFrame__gameGirl">GAME GIRL</div>
+            <div className="gameBoyFrame__gameGirl">GAME GIRL</div> */}
 
             <div className="pokemonCardContainer">
               {pokemonData.map((pokemon, i) => {
@@ -100,6 +275,17 @@ function App() {
         <div className="btn">
           <button onClick={handlePrevPage}>まえへ</button>
           <button onClick={handleNextPage}>つぎへ</button>
+          <button onClick={handleGoldSilverPage}>金・銀</button>
+          <button onClick={handleRubySapphirePage}>ルビ・サファ</button>
+          <button onClick={handleDiamondPearl}>ダイ・パ</button>
+          <button onClick={handleBlackWhite}>黒・白</button>
+          <button onClick={handleXY}>X・Y</button>
+          <button onClick={handleSunMoon}>サン・ムーン</button>
+          <button onClick={handleSwordShield}>剣・盾</button>
+          <button onClick={handleArceus}>アルセウス</button>
+          <button onClick={handleScarletViolet}>
+            スカーレット・バイオレット
+          </button>
         </div>
       </div>
     </>
